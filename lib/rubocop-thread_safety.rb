bc10117b4d7a4db284ef3b56bffce3f5c3fd284a
Igor Zubkov <igor.zubkov@gmail.com>
2017-10-20 00:15:42 +0300

    WIP

--- lib/rubocop-thread_safety.rb
+++ lib/rubocop-thread_safety.rb
@@ -1,7 +1,11 @@
 require 'rubocop'
 
+require 'rubocop/thread_safety'
+require 'rubocop/thread_safety/inject'
 require 'rubocop/thread_safety/version'
 
 require 'rubocop/cop/thread_safety/instance_variable_in_class_method'
 require 'rubocop/cop/thread_safety/class_and_module_attributes'
 require 'rubocop/cop/thread_safety/new_thread'
+
+RuboCop::ThreadSafety::Inject.defaults!
